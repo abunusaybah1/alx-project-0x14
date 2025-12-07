deeb85bc1bda0a966a900b3513a9c0defe839a65
abunusaybah <your_github_email@example.com>
2025-12-07 21:36:43 +0100

    Task 2

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { ButtonProps } from "@/interfaces";
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
+	return (
+		<button
+			onClick={action}
+			className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300">
+			{title}
+		</button>
+	);
+};
 
-export default Button
+export default Button;
